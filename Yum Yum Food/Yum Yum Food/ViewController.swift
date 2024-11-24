209303e9767d8693d3daa2c4774dbffc3a6424c5
drulik44 <sahanzxi984325qx@gmail.com>
2024-11-24 02:08:29 +0200

    created base project, remove storyboard

---
+++ Yum Yum Food/Yum Yum Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yum Yum Food
+//
+//  Created by Руслан Жидких on 24.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
